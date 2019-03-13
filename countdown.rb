6a68620628f40f7d343cadf5eb92e2c5fe2dfe69
Michelle M <michellemoon236@gmail.com>
2019-03-13 21:35:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown (number)
   while number > 0 
     puts "#{number} SECOND(S)!"
     number -=1
-    def countdown_with_sleep 
+    def countdown_with_sleep (number)
       sleep 1
     end
   end
